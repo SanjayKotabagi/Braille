96cb10448bcc2f6f495d728683ef1ac11cc9849b
Sanjay Kotabagi <sanjaymkotabagi@gmail.com>
2023-04-17 08:19:29 +0530

    Add files via upload

---
+++ translitterator/PATHS.py
@@ -0,0 +1,14 @@
+NAVBAR_PATHS = {
+    'HOME':'home',
+    'ABOUT': 'about',
+    'Marati To Braille' : 'mar2br',
+    'English To Braille' : 'eng2br',
+    'Kannada To Braille': 'kan2br',
+    'Telugu To Braille' : 'tel2br',
+    
+}
+
+SETTINGS = {
+    'OPTIONS':'options',
+    'CONFIGURATION':'configuration'
+}
